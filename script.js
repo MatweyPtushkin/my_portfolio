e231fd3da8271cb1923d15231d9aa1fd565adc8c
MatweyPtushkin <m.ptushkin@inbox.ru>
2020-06-30 00:48:20 +0300

    delete errors

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-$(document).ready(function ($) {
+$(document).ready(function () {
   $(".my_slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
   });
-});
 
+  $(".switch-button").click(function () {
+    if ($(".sidebar").hasClass("active")) {
+      $(".sidebar").hide(700);
+      $(".sidebar").removeClass("active");
+      $(".switch-button img").attr("src", "images/arrow-right.png");
+    } else {
+      $(".sidebar").show(700);
+      $(".sidebar").addClass("active");
+      $(".switch-button img").attr("src", "images/arrow-left.png");
+    }
+  });
+});
